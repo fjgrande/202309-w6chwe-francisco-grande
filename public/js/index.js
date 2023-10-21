dbbb1b5e2bc92ac878516a211795d91ac4227053
Francisco Grande <fjgrandv@gmail.com>
2023-10-21 15:45:26 +0200

    Fix compilated files js

--- public/js/index.js
+++
@@ -1,5 +0,0 @@
-import App from "./components/App/App.js";
-const parentElement = document.querySelector(".app");
-const app = new App(parentElement);
-app.render();
-//# sourceMappingURL=index.js.map
