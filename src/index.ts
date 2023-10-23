8f941025406874c72258c6125effc4ad0f3541fd
Francisco Grande <fjgrandv@gmail.com>
2023-10-23 02:57:44 +0200

    Add url api pokemon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import App from "./components/App/App.js";
+import getPokemonApi from "./data/getPokemonApi.js";
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=10";
+await getPokemonApi(url);
 
 const parentElement = document.querySelector("body")!;
 const app = new App(parentElement);
